426753f7fcfa5315a7dccad15edcf3c56ed84b12
Aleksandr Chadaev <chedosaf@mail.ru>
2023-09-06 17:26:07 +0400

    chore: clean up README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react'
+import './App.css'
 
-function App() {
+const App = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="header">Hello</div>
+      <div className="footer">World</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
