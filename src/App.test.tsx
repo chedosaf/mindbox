c7c349218222a6e320c9ee033c2addb32d6b1cf3
Aleksandr Chadaev <chedosaf@mail.ru>
2023-09-11 16:50:40 +0400

    feat: add todo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import i18n from 'i18next'
+import { I18nextProvider, initReactI18next } from 'react-i18next'
+import { render, screen } from '@testing-library/react'
+
+import App from './App'
+import resources from './locales/index'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  i18n.use(initReactI18next).init({
+    resources,
+    lng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+  const { getByText } = render(
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>,
+  )
+  const linkElement = getByText(/todo list/i)
+  expect(linkElement).toBeInTheDocument()
+})
---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Checkbox } from 'antd'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding: 10px;
+  background: #858584;
+  color: #f2f2f2;
+  border: 1px solid #656564;
+  border-radius: 4px;
+  margin: 6px 0;
+  display: flex;
+`
+const TextWrapper = styled.div`
+  white-space: normal;
+  overflow: scroll;
+  overflow-wrap: anywhere;
+`
+
+interface TaskProps {
+  text: string
+  status: string
+  id: string
+  handleChange: (id: string) => void
+}
+
+const Task = ({ text, status, id, handleChange }: TaskProps) => {
+  return (
+    <Wrapper
+      style={{
+        textDecoration: status === 'complited' ? 'line-through' : 'none',
+      }}
+    >
+      <div style={{ margin: '0 12px' }}>
+        <Checkbox
+          checked={status === 'complited'}
+          onChange={() => handleChange(id)}
+        />
+      </div>
+      <TextWrapper>{text}</TextWrapper>
+    </Wrapper>
+  )
+}
+
+export default Task
